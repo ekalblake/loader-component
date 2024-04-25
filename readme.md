15a9c2d0785c3ef32d78978ce1cbc785b288a862
Paolo Villena <p.villena.c38@gmail.com>
2024-04-25 01:17:55 -0500

    remove comments + readme.md added

---
+++ readme.md
@@ -0,0 +1,29 @@
+# Loader Component
+
+Componente loader que cubre toda la pantalla al momento de inicializarse.
+
+## Requisitos Previos
+
+Herramientas instaladas previamente:
+
+- Node.js y npm
+- Angular CLI
+- Ionic CLI
+
+## Instalación
+
+1. Clonar el repositorio
+
+   ```bash
+   git clone https://github.com/ekalblake/loader-component.git
+   
+2. Navegar
+
+   ```bash
+   cd loader-component
+
+3. Instalar dependencias
+
+    ```bash
+    npm install
+
--- src/pages/home/home.service.ts
+++ src/pages/home/home.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeService {
-  constructor(private router: Router) {}
+  constructor() {}
 
   callApi(miliseconds: number) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        // Resolviendo la promesa con los datos de los usuarios
         resolve('Accediendo a la otra página');
       }, miliseconds);
     });
